58c98aae4452418854a0277f1b71645abc87f5d0
Millennial Investments <admin@mymillennialinvestments.com>
2021-01-07 23:19:18 -0600

    v5 Update

---
+++ dist/admin/js/global.js
@@ -0,0 +1,27 @@
+/* Nofication Close Buttons */
+$('.notification a.close').click(function(e){
+	e.preventDefault();
+
+	$(this).parent('.notification').fadeOut();
+});
+
+/*
+	Check All Feature
+*/
+$(".check-all").click(function(){
+	$("table input[type=checkbox]").attr('checked', $(this).is(':checked'));
+});
+/*
+	Set focus on the first form field
+*/
+$(":input:visible:first").focus();
+
+/*
+	Responsive Navigation
+*/
+$('.collapse').collapse();
+
+/*
+ Prevent elements classed with "no-link" from linking
+*/
+//$(".no-link").click(function(e){ e.preventDefault();	});
